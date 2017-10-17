454c2e8fc22cbc37364eb541dbba3d20acb91fb7
Shah RUtul <shahrutul111@gmail.com>
2017-10-17 11:02:44 +0530

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,82 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!([
+  {
+    email: "ajil@shopknekt.com",
+    encrypted_password: "$2a$11$/2XxIJhk6JakRKQ3W9L3LOgSvS7msCYRlrPbqm62O69eNsWoRNDiC", 
+    reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 2, current_sign_in_at: "2017-10-10 17:05:36", last_sign_in_at: "2017-10-10 17:05:19", current_sign_in_ip: "::1", last_sign_in_ip: "::1", image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil
+    },
+  {
+    email: "admin@gmail.com",
+    encrypted_password: "$2a$11$FcdPLZFV79n1Nmcl/JJNXOJtA3UU.x95iF9/ltzi/8oERhkjZpq.S", 
+    reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 1, current_sign_in_at: "2017-10-12 10:26:59", last_sign_in_at: "2017-10-12 10:26:59", current_sign_in_ip: "::1", last_sign_in_ip: "::1", image_file_name: nil, image_content_type: nil, image_file_size: nil, image_updated_at: nil
+  }
+  ])
+About.create!([
+  {
+    title: "About",
+    description: "<p>We&nbsp;are&nbsp;one&nbsp;of&nbsp;the&nbsp;leading&nbsp;private&nbsp;security&nbsp;agencies&nbsp;in&nbsp;Bangalore.&nbsp;We&nbsp;also&nbsp;list&nbsp;in&nbsp;top&nbsp;20&nbsp;best&nbsp;security&nbsp;</p>\r\n\r\n<p>agency&nbsp;in&nbsp;Bangalore.&nbsp;We&nbsp;provide&nbsp;Commercial&nbsp;security&nbsp;services,&nbsp;Apartment&nbsp;security&nbsp;guard&nbsp;services,&nbsp;</p>\r\n\r\n<p>residential&nbsp;security&nbsp;guards,&nbsp;school&nbsp;security&nbsp;services,&nbsp;security&nbsp;services&nbsp;for&nbsp;hotels,&nbsp;security&nbsp;services&nbsp;for&nbsp;</p>\r\n\r\n<p>hospital,&nbsp;Industrial&nbsp;security&nbsp;services,&nbsp;security&nbsp;services&nbsp;for&nbsp;factory,&nbsp;security&nbsp;services&nbsp;for&nbsp;jewelry&nbsp;</p>\r\n\r\n<p>showrooms.&nbsp;&nbsp;</p>\r\n\r\n<p>BlackStone&nbsp;Security&nbsp;is&nbsp;a&nbsp;leading&nbsp;security&nbsp;services&nbsp;company/agency&nbsp;offering&nbsp;security&nbsp;</p>\r\n\r\n<p>services&nbsp;in&nbsp;Marathalli,&nbsp;Whitefield,&nbsp;Bellandur,&nbsp;Jayanagar,&nbsp;JP&nbsp;Nagar&nbsp;and&nbsp;across&nbsp;Bangalore&nbsp;areas.</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>We&nbsp;are&nbsp;one&nbsp;of&nbsp;the&nbsp;most&nbsp;trusted&nbsp;names&nbsp;in&nbsp;Security&nbsp;Services&nbsp;industry&nbsp;in&nbsp;</p>\r\n\r\n<p>Bangalore.&nbsp;Our&nbsp;service&nbsp;expertise&nbsp;includes&nbsp;security&nbsp;service&nbsp;for&nbsp;Hotels,&nbsp;</p>\r\n\r\n<p>security&nbsp;service&nbsp;for&nbsp;Schools&nbsp;and&nbsp;Apartment&nbsp;security&nbsp;guard&nbsp;service&hellip;&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>BlackStone&nbsp;Security&nbsp;is&nbsp;top&nbsp;security&nbsp;services&nbsp;Company/Agency&nbsp;in&nbsp;Bangalore&nbsp;</p>\r\n\r\n<p>offering&nbsp;security&nbsp;service&nbsp;for&nbsp;Hotels,&nbsp;security&nbsp;service&nbsp;for&nbsp;Schools&nbsp;and&nbsp;</p>\r\n\r\n<p>security&nbsp;guard&nbsp;service&nbsp;for&nbsp;Apartments&hellip;&nbsp;</p>\r\n", image_file_name: "1441469417-bagmane-tech-park-location-cv-raman-nagar.jpg",
+    image_content_type: "image/jpeg", image_file_size: 81431, image_updated_at: "2017-10-10 17:14:30"
+    },
+  {
+    title: "OurMission",
+    description: "To extend our value-added security services to quality-conscious clients who consider security an equal important subject by making security services as a very organized business services. we want to give you a flavor of BlackStone’s character as a company, an overview of the way we do business with our clients, and how we select and develop our people. We make our clients feel secure, valued, and part of the BlackStone Security team. It is feature of the way we do business that our Exservicemen personnel take personal responsibility for our clients, their requirements, and the business relationships. Since BlackStone Security was founded, we’ve been privileged to act as the guardians. We understand our client’s needs and challenges and deliver, time and time again. We are constantly striving to raise the industry’s standards and transforming security services from unorganized to organized business services.", image_file_name: "mission.png",
+    image_content_type: "image/png", image_file_size: 514868, image_updated_at: "2017-10-12 10:37:32"
+    },
+  {
+    title: "BusinessPrinciples",
+    description: "These principles guide our business strategies, our ethical behaviour, and our relationships with customers, communities, and each other. BlackStone Security offers a wide variety of security services to Corporates, Hotels and Restaurants, Schools, Construction sites, Retail facilities, Hospitals, Apartments, Residences, and Shopping Malls.", 
+    image_file_name: "bp.jpg",
+    image_content_type: "image/jpeg", image_file_size: 24573, image_updated_at: "2017-10-12 10:38:36"
+  }
+  ])
+Service.create!([
+  {
+    title: "Security Services",
+    short_description: "We take real pride in delivering the highest levels of service to our prestigious client base. With many years of industry expertise and knowledge, we ensure the right solution is delivered every time.", 
+    description: "<p>We&nbsp;take&nbsp;real&nbsp;pride&nbsp;in&nbsp;delivering&nbsp;the&nbsp;highest&nbsp;levels&nbsp;of&nbsp;service&nbsp;to&nbsp;our&nbsp;prestigious&nbsp;</p>\r\n\r\n<p>client&nbsp;base.&nbsp;With&nbsp;many&nbsp;years&nbsp;of&nbsp;industry&nbsp;expertise&nbsp;and&nbsp;knowledge,&nbsp;we&nbsp;ensure&nbsp;</p>\r\n\r\n<p>the&nbsp;right&nbsp;solution&nbsp;is&nbsp;delivered&nbsp;every&nbsp;time.</p>\r\n", 
+    image_file_name: "Security_Services.jpg",
+    image_content_type: "image/jpeg",
+    image_file_size: 133616, image_updated_at: "2017-10-10 17:08:19"
+    },
+  {
+    title: "SecurityServices",
+    short_description: "We take real pride in delivering the highest levels of service to our prestigious client base.\r\n\r\nWith many years of industry expertise and knowledge, we ensure the right solution is delivered every time.",
+    description: "We take real pride in delivering the highest levels of service to our prestigious client base.\r\n\r\nWith many years of industry expertise and knowledge, we ensure the right solution is delivered every time.",
+    image_file_name: "Security_Services.jpg",
+    image_content_type: "image/jpeg",
+    image_file_size: 133616, image_updated_at: "2017-10-12 11:46:43"
+    },
+  {
+    title: "Security Services",
+    short_description: "We take real pride in delivering the highest levels of service to our prestigious client base.\r\n\r\nWith many years of industry expertise and knowledge, we ensure the right solution is delivered every time.",
+    description: "We take real pride in delivering the highest levels of service to our prestigious client base.\r\n\r\nWith many years of industry expertise and knowledge, we ensure the right solution is delivered every time.",
+    image_file_name: "Security_Services.jpg",
+    image_content_type: "image/jpeg",
+    image_file_size: 133616, image_updated_at: "2017-10-12 12:01:51"
+    },
+  {
+    title: "ReceptionServices",
+    short_description: "We take real pride in delivering the highest levels of service to our prestigious client base.\r\n\r\nWith many years of industry expertise and knowledge, we ensure the right solution is delivered every time.",
+    description: "<p>We take real pride in delivering the highest levels of service to our prestigious client base. With many years of industry expertise and knowledge, we ensure the right solution is delivered every time.</p>\r\n",
+     image_file_name: "Security_Services.jpg",
+     image_content_type: "image/jpeg",
+     image_file_size: 133616, image_updated_at: "2017-10-12 11:35:57"
+   }
+  ])
+Testimonial.create!([
+  {
+    name: "dksjdkljslk ", 
+    description: ".kfdlfkjlkfjdlkfjdlkfjdlkfjldk dklfjlkdj fdskfljslk fldkjf", 
+    place: "djlkfj", 
+    image_file_name: "C.jpg", 
+    image_content_type: "image/jpeg", 
+    image_file_size: 10038, 
+    image_updated_at: "2017-10-10 17:29:13"},
+  {
+    name: "fkjfkj", 
+    description: "kjldfkjflkdjf flkdjlfkjdlf ldfjdlkfjldkjflkj", 
+    place: "dfjdfkdj", 
+    image_file_name: "Construction-Site-.jpg", 
+    image_content_type: "image/jpeg", 
+    image_file_size: 1464687, 
+    image_updated_at: "2017-10-10 17:29:43"}
+  ])
